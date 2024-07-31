9e922d4ec223e3f26feb42b843ce6981c0b683ed
Akshay Sharma <akshaysharma.2993@gmail.com>
2024-07-31 16:39:10 +0530

    Add changes to contact form css file

--- src/components/Home/Social.jsx
+++ src/components/Home/Social.jsx
@@ -9,7 +9,12 @@ const Social = () => {
     const ctx = gsap.context(() => {
       tl.current = gsap
         .timeline()
-        .from(".home__social--inner", { opacity: 0,left:'-500px', duration: 1, ease: "back" })
+        .from(".home__social--inner", {
+          opacity: 0,
+          left: "-500px",
+          duration: 1,
+          ease: "back",
+        })
         .from(".home__social-icon", {
           x: "-500px",
           ease: "back",
@@ -22,16 +27,28 @@ const Social = () => {
   return (
     <div ref={social} className="home__social">
       <div className="home__social--inner">
-        <a href="https://www.linkedin.com/in/akshay-sharmaaa/" className="home__social-icon" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/akshay-sharmaaa/"
+          className="home__social-icon"
+          target="_blank"
+        >
           <i className="uil uil-linkedin"></i>
         </a>
-        <a href="https://github.com/akshay2993" className="home__social-icon" target="_blank">
+        <a
+          href="https://github.com/akshay2993"
+          className="home__social-icon"
+          target="_blank"
+        >
           <i className="uil uil-github"></i>
         </a>
         {/*<a href="" className="home__social-icon" target="_blank">
           <i className="uil uil-facebook-f"></i>
         </a>*/}
-        <a href="https://www.instagram.com/akshay_01_/" className="home__social-icon" target="_blank">
+        <a
+          href="https://www.instagram.com/akshay_01_/"
+          className="home__social-icon"
+          target="_blank"
+        >
           <i className="uil uil-instagram"></i>
         </a>
       </div>
